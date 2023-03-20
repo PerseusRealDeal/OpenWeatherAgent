f12132982420a5892fa76f0698869e3daf129f04
Mikhail <mzhigulin@gmail.com>
2023-03-20 12:53:18 +0700

    PUT: OpenWeather client in single file
    This commit goes with updated [OpenWeatherStar.swift].

--- OpenWeatherStar.swift
+++ OpenWeatherStar.swift
@@ -33,9 +33,171 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 //
-// swiftlint_:disable file_length
+// swiftlint:disable file_length closure_parameter_position
 //
 
-struct OpenWeatherFreeClient {
-    var text = "Hello, World!"
+import Foundation
+
+class OpenWeatherFreeClient: FreeNetworkClient {
+
+    func call(with respect: OpenWeatherDetails) throws {
+        guard let requestURL = URL(string: respect.urlString) else {
+            throw NetworkClientError.invalidUrl
+        }
+
+        requestData(url: requestURL)
+    }
+}
+
+public enum NetworkClientError: Error, Equatable {
+    case invalidUrl
+    case failedRequest(String)
+}
+
+public enum Result<Value, Error: Swift.Error> {
+    case success(Value)
+    case failure(Error)
+}
+
+class FreeNetworkClient {
+
+    private(set) var dataTask: URLSessionDataTask?
+    private(set) var session: URLSession
+
+    var onDataGiven: (Result<Data, NetworkClientError>) -> Void = { result in
+        #if DEBUG
+        print(#function + ": \(result)")
+        #endif
+    }
+
+    var data: Data { return networkData }
+    private(set) var networkData: Data = Data() {
+        didSet {
+            onDataGiven(.success(networkData))
+        }
+    }
+
+    init(_ session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+
+    internal func requestData(url: URL) {
+
+        dataTask = session.dataTask(with: URLRequest(url: url)) {
+            [self] (requestedData: Data?, response: URLResponse?, error: Error?) -> Void in
+
+            // Read answer
+
+            var answerData: Data?
+            var answerError: String?
+
+            if let error = error {
+                answerError = error.localizedDescription
+            } else if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                answerError = HTTPURLResponse.localizedString(
+                    forStatusCode: response.statusCode)
+            } else if let requestedData = requestedData {
+                answerData = requestedData
+            }
+
+            // Communicate changes
+
+            DispatchQueue.main.async { [weak self] in
+
+                guard let self = self else { return }
+
+                if let requestedData = answerData {
+                    self.networkData = requestedData
+                } else if let error = answerError {
+                    self.onDataGiven(.failure(.failedRequest(error)))
+                }
+
+                self.dataTask = nil
+            }
+        }
+
+        dataTask?.resume()
+    }
+}
+
+let weatherSchemeBase = "https://api.openweathermap.org/data/2.5/"
+let weatherSchemeAttributes = "%@?lat=%@&lon=%@&appid=%@"
+
+enum OpenWeatherURLFormat: String {
+    case currentWeather = "weather"
+    case forecast = "forecast"
+}
+
+enum Units: String {
+    case standard // By default.
+    case metric
+    case imperial
+}
+
+enum Mode: String {
+    case json // By default.
+    case xml
+    case html
+}
+
+struct Lang: RawRepresentable {
+    var rawValue: String
+    static let byDefault = Lang(rawValue: "") // By default lang attribute is empty.
+}
+
+extension Lang {
+    static let en = Lang(rawValue: "en")
+    static let ru = Lang(rawValue: "ru")
+}
+
+struct OpenWeatherDetails {
+
+    let appid: String
+    let format: OpenWeatherURLFormat
+
+    let lat: String
+    let lon: String
+
+    let units: Units
+    let lang: Lang
+    let mode: Mode
+
+    var cnt: Int = -1 // A number of timestamps, which will be returned in the API response.
+
+    init(appid: String, format: OpenWeatherURLFormat = .currentWeather,
+         lat: String = "55.66", lon: String = "85.62", units: Units = .standard,
+         lang: Lang = Lang.byDefault, mode: Mode = Mode.json) {
+
+        self.appid = appid
+        self.format = format
+        self.lat = lat
+        self.lon = lon
+        self.units = units
+        self.lang = lang
+        self.mode = mode
+    }
+
+    var urlString: String {
+
+        let args: [String] = [format.rawValue, lat, lon, appid]
+        var attributes = String(format: weatherSchemeAttributes, arguments: args)
+
+        if !lang.rawValue.isEmpty {
+            attributes.append("&lang=\(lang.rawValue)")
+        }
+
+        if format == .forecast && cnt != -1 {
+            attributes.append("&cnt=\(cnt)")
+        }
+
+        if mode != .json {
+            attributes.append("&mode=\(mode.rawValue)")
+        }
+
+        if units != .standard {
+            attributes.append("&units=\(units.rawValue)")
+        }
+
+        return weatherSchemeBase + attributes
+    }
 }
